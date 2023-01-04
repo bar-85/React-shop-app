95a883be9c3f502150adf5b52190c5e7ae66c505
bar-85 <bajwolskabarbara@gmail.com>
2023-01-04 23:51:24 +0100

    Delete picture__orange

--- src/components/Layout/Home.js
+++ src/components/Layout/Home.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react'
 import { motion } from 'framer-motion'
 import classes from './Home.module.css'
-import orange from '../../assets/orange.png'
 import bannerImg from '../../assets/imgbanner.png'
 
 const Home = () => {
@@ -16,14 +15,8 @@ const Home = () => {
 					<p>Zapraszamy do zakupów</p>
 				</div>
 				<div className={classes.picture}>
-					<img className={classes.picture__orange} src={orange} alt='pomarańczowe tło' />
 					<motion.div animate={{ y: [-350, -250, -350] }} transition={{ duration: 5, type: 'tween', repeat: Infinity }}>
-						<img
-							className={classes.picture__bee}
-							style={{ height: '15rem', width: 'auto', zIndex: '2', right: '0rem' }}
-							src={bannerImg}
-							alt='pszczoła'
-						/>
+						<img className={classes.picture__bee} src={bannerImg} alt='pszczoła' />
 					</motion.div>
 				</div>
 			</div>
