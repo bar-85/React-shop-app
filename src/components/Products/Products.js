91f4777c47ad77054f266a45ea5b422ada0c16a2
bar-85 <bajwolskabarbara@gmail.com>
2023-01-06 22:22:05 +0100

    Add Products.js

---
+++ src/components/Products/Products.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import ProductsTitle from './ProductsTitle'
+import ProductsList from './ProductsList'
+
+const Products = () => {
+	return (
+		<Fragment>
+			<ProductsTitle />
+			<ProductsList />
+		</Fragment>
+	)
+}
+
+export default Products
