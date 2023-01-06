9b9b96efabb80466e9fc444ceeed27623e95e82f
bar-85 <bajwolskabarbara@gmail.com>
2023-01-06 22:22:33 +0100

    Add ProductList.js and css

---
+++ src/components/Products/ProductsList.js
@@ -0,0 +1,49 @@
+import classes from './ProductsList.module.css'
+import Card from '../UI/Card'
+import ProductItem from './ProductItem/ProductItem'
+import honey1 from '../../assets/honey1.png'
+import honey2 from '../../assets/honey2.png'
+import honey3 from '../../assets/honey3.png'
+import honey4 from '../../assets/honey4.png'
+
+
+export const DUMMY_PRODUCTS = [
+	{
+		id: 1,
+		name: 'Miód wielokwiatowy',
+		price: 50,
+		imgUrl: honey1,
+	},
+	{
+		id: 2,
+		name: 'Pyłek kwiatowy',
+		price: 75,
+		imgUrl: honey2,
+	},
+	{
+		id: 3,
+		name: 'Plaster miodu',
+		price: 100,
+		imgUrl: honey3,
+	},
+	{
+		id: 4,
+		name: 'Pszczoły miodne',
+		price: 500,
+		imgUrl: honey4,
+	},
+]
+
+const ProductsList = () => {
+	const prosuctsList = DUMMY_PRODUCTS.map(product => <ProductItem key={product.id} name={product.name} price={product.price} imgUrl={product.imgUrl} />)
+
+	return (
+		<section className={classes.products}>
+			<Card>
+				<ul>{prosuctsList}</ul>
+			</Card>
+		</section>
+	)
+}
+
+export default ProductsList
