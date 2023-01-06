b0c4d2e1113e7e23d94c4fb0fd01efb4fee134c8
bar-85 <bajwolskabarbara@gmail.com>
2023-01-06 22:22:51 +0100

    Add ProductTitle.js and css

---
+++ src/components/Products/ProductsTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './ProductsTitle.module.css'
+
+const ProductsTitle = () => {
+	return (
+		<section className={classes.title}>
+			<h2>Lista produktów</h2>
+		</section>
+	)
+}
+
+export default ProductsTitle
