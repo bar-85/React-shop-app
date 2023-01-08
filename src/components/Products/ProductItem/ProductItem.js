1bacb87663d107f9a0a132aae35e61faa35666bc
bar-85 <bajwolskabarbara@gmail.com>
2023-01-09 00:16:15 +0100

    Add CartContext to ProductItem.js

--- src/components/Products/ProductItem/ProductItem.js
+++ src/components/Products/ProductItem/ProductItem.js
@@ -1,21 +1,34 @@
+import { useContext } from 'react'
 import classes from './ProductItem.module.css'
 import ProductItemForm from './ProductItemForm'
-
+import CartContext from '../../../store/cart-context'
 
 const ProductItem = props => {
-    const price = `zł ${props.price.toFixed(2)}`
+    const cartCtx = useContext(CartContext)
+	const price = `zł ${props.price.toFixed(2)}`
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            imgUrl: props.imgUrl,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
+
 	return (
 		<li className={classes.product}>
 			<div>
-                <div className={classes.imgUrl} >
-                    <img src={props.imgUrl} alt="" />
-                </div>
+				<div className={classes.imgUrl}>
+					<img src={props.imgUrl} alt='' />
+				</div>
 				<h3>{props.name}</h3>
-                <div className={classes.price}>{price}</div>
+				<div className={classes.price}>{price}</div>
 			</div>
-            <div>
-                <ProductItemForm />
-            </div>
+			<div>
+				<ProductItemForm onAddToCart={addToCartHandler}/>
+			</div>
 		</li>
 	)
 }
