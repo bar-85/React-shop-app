ea22342383d81a218d445c1e731a2b01630444a8
bar-85 <bajwolskabarbara@gmail.com>
2023-01-09 00:13:51 +0100

    Add Cart and CartProvider to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import { Fragment } from 'react'
+import { useState } from 'react'
 
 import Header from './components/Layout/Header'
 import Home from './components/Layout/Home'
 import Products from './components/Products/Products'
 import Arrow from './components/Layout/Arrow'
+import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
 
 function App() {
+	const [cartShow, setCartShow] = useState(false)
+
+	const showCartHandler = () => {
+		setCartShow(true)
+	}
+
+	const hideCartHandler = () => {
+		setCartShow(false)
+	}
+
 	return (
-		<Fragment>
-			<Header />
+		<CartProvider>
+			{cartShow && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
 			<main>
 				<Home />
 				<Arrow />
 				<Products />
 			</main>
-		</Fragment>
+		</CartProvider>
 	)
 }
 
