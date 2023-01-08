32de2a00d5e2034a0ef7075961756addd5e9f797
bar-85 <bajwolskabarbara@gmail.com>
2023-01-09 00:17:48 +0100

    Add Hooks to ProductItemForm.js

--- src/components/Products/ProductItem/ProductItemForm.js
+++ src/components/Products/ProductItem/ProductItemForm.js
@@ -1,20 +1,45 @@
+import { useRef, useState } from 'react'
 import Input from '../../UI/Input'
 import classes from './ProductItemForm.module.css'
 
-const ProductItemForm = (props) => {
-  return (
-    <form className={classes.form}>
-        <Input className={classes.input} label='Ilość:' input={{
-            id: 'amount_' + props.id,
-            type: 'number',
-            min: '1',
-            max: '10',
-            step: '1',
-            defaultValue: '1',
-        }}/>
-        <button>+</button>
-    </form>
-  )
+const ProductItemForm = props => {
+	const [amountIsValid, setAmountIsValid] = useState(true)
+
+	const amountInputRef = useRef()
+
+	const submitHandler = e => {
+		e.preventDefault()
+
+		const enteredAmount = amountInputRef.current.value
+		const enterdAmountNumber = +enteredAmount
+
+		if (enteredAmount.trim().length === 0 || enterdAmountNumber < 1 || enterdAmountNumber > 10) {
+			setAmountIsValid(false)
+			return
+		}
+
+		props.onAddToCart(enterdAmountNumber)
+	}
+
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<Input
+				ref={amountInputRef}
+				className={classes.input}
+				label='Ilość:'
+				input={{
+					id: 'amount_' + props.id,
+					type: 'number',
+					min: '1',
+					max: '10',
+					step: '1',
+					defaultValue: '1',
+				}}
+			/>
+			<button>+</button>
+			{!amountIsValid && <p>(1-10)</p>}
+		</form>
+	)
 }
 
 export default ProductItemForm
