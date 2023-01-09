865ee88fe49a223ba80d7af958fa5f8d35c76df2
bar-85 <bajwolskabarbara@gmail.com>
2023-01-09 22:53:40 +0100

    Add btnJump to button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = props => {
+	const [btnJump, setbtnJump] = useState(false)
 	const cartCtx = useContext(CartContext)
 
-	const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
+	const { items } = cartCtx
+
+	const numberOfCartItem = items.reduce((curNumber, item) => {
 		return curNumber + item.amount
 	}, 0)
 
+	const btn = `${classes.button} ${btnJump ? classes.jump : ''}`
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return
+		}
+		setbtnJump(true)
+
+		const timer = setTimeout(() => {
+			setbtnJump(false)
+		}, 300)
+
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [items])
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btn} onClick={props.onClick}>
 			<span>🛒</span>
 			<div className={classes.badge}>{numberOfCartItem}</div>
 		</button>
