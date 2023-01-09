f743137f7e87ec5020c05ed9746ac08e36505eb2
bar-85 <bajwolskabarbara@gmail.com>
2023-01-09 22:30:38 +0100

    Add scrollTo to logo

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,10 +4,14 @@ import classes from './Header.module.css'
 import HeaderCartButton from './HeaderCartButton'
 
 const Header = props => {
+	function handleClick() {
+		window.scrollTo(0, 0);
+	  }
+
 	return (
 		<Fragment>
 			<header className={classes.header}>
-				<img src={logo} alt='logo sklepu miodek' className={classes.logo} />
+				<img src={logo} alt='logo sklepu miodek' className={classes.logo} onClick={handleClick} />
 				<h1>
 					Pasieka <span>"Miodek"</span>
 				</h1>
