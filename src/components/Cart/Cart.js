9c56a9a01578e6d89e0983ba093b96457645747c
bar-85 <bajwolskabarbara@gmail.com>
2023-01-09 22:00:37 +0100

    Add Remove and add Handler to Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,17 +11,25 @@ const Cart = props => {
 	const hasItems = cartCtx.items.length > 0
 
 	const cartItemRemoveHandler = id => {
-
+		cartCtx.removeItem(id)
 	}
 
 	const cartItemAddHandler = item => {
-
+		cartCtx.addItem({ ...item, amount: 1 })
 	}
 
 	const cartItems = (
 		<ul className={classes['cart-items']}>
 			{cartCtx.items.map(item => (
-				<CartItem key={item.id} imgUrl={item.imgUrl} name={item.name} amount={item.amount} price={item.price} onRemowe={cartItemRemoveHandler} onAdd={cartItemAddHandler}/>
+				<CartItem
+					key={item.id}
+					imgUrl={item.imgUrl}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	)
